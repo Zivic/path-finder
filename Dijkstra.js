91c387795fc101da8fbe6869534be2b371ab1687
Djordje Zivic <djzivic@gmail.com>
2020-12-02 02:16:23 +0100

    EZ Clap

--- Dijkstra.js
+++ Dijkstra.js
@@ -20,11 +20,15 @@
         let y=5;
         let iterations = 0;
 
-        var promise2 = null;
+        
+    
         while (Q.size != 0 && iterations <10){
             //TODO: This  is hacky, fix it with a proper promise after "promise" finishes
-            await new Promise(r => setTimeout(r, 4000));
-            //await promise2;
+           // await new Promise(r => setTimeout(r, 4000));
+            var promise2 = new Promise((resolve,reject) => {
+
+
+//await promise2;
             iterations++;
             
             //TODO:pomeri ovo gore ?? ova 2 reda
@@ -41,7 +45,8 @@
             Q.delete(currentMinimalNode.value);
 
             //if(u.hasGoal()) break;
-            if(currentMinimalNode.isEndNode === true) break;
+            //TODO: Vrati ovo
+            //if(currentMinimalNode.isEndNode === true) break;
             //TODO: pozovi generateneighbors za  currentMinimalnode
 
             //forEach neighbor v in u{
@@ -50,11 +55,25 @@
 
             let interval = 1000;
             var promise = Promise.resolve();
+            let itemsProcessed = 0;
+            let numberOfNonVisitedNodes = 0;
+            let nonVisitedNodes = [];
 
             currentMinimalNode.value.neighbours.forEach((neighborNode) => {
+                if(neighborNode.visited === false){
+                numberOfNonVisitedNodes++;
+                nonVisitedNodes.push(neighborNode);
+                }
+            })
+
+            nonVisitedNodes.forEach((neighborNode, index, array) => {
+                console.log("UUUUUUUUUU")
+                //itemsProcessed++;
+                //if(neighborNode.visited === false)
                 promise = promise.then(() => {
+                    
 //SVE               console.log('pppp');
-           console.log(neighborNode);
+           //console.log(neighborNode);
            neighborNode.distance = currentMinimalNode.value.distance+1;
            neighborNode.visitNode();
            
@@ -68,12 +87,28 @@
             }
                     return new Promise(resolve => setTimeout(resolve, interval));
                 })
+                .then(() => {
+                    itemsProcessed++
+                    console.log("items processed:")
+                    console.log(itemsProcessed);
+
+                    if(itemsProcessed === array.length ){
+                        console.log("resolved");
+                        resolve();
+                    }
+                })
            // alt = dist[u]+ length(u,v) //length je 1 za direkntog neoghboura i sabira se za ostale
            
            //promise.then(() => promise2.resolve());
 
-
+            
+            
         })
+ 
+
+            })
+            await promise2;
+            
         
         }
         console.log("Dijkstra complete!");
